a42e65dbc2a70bc301dad18b536df4740e77504a
Promise Mwenga <promise_mwenga@hotmail.com>
2022-08-30 14:59:02 -0300

    merged 02 flask form

--- 01_Flask_Forms/01_flask_forms.py
+++ 01_Flask_Forms/01_flask_forms.py
@@ -12,4 +12,6 @@ def index():
 @app.route("/greet", methods=["POST", "GET"])
 def greet():
     flash("Greetings " + str(request.form['name_input']) + ", the light welcomes you.")
+    flash("Nice you live at " + str(request.form['address_input']) + ", may light warm  your home..")
+
     return render_template("index.html")
