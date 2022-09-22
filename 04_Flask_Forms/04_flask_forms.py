1b714dc1bae7449b95d574d8dccee1569ac01355
Promise Mwenga <promise_mwenga@hotmail.com>
2022-09-22 00:52:33 -0300

    added errors flashing and corrected clercal errors

--- 04_Flask_Forms/04_flask_forms.py
+++ 04_Flask_Forms/04_flask_forms.py
@@ -12,15 +12,21 @@ from os.path import exists
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'SUPER-seCRet-Key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///myUserDB.db'
-app.config['SQLALCHEMY_TRACK_MODIFICITIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
-    password = db.Column(db.String)
+    password_hash = db.Column(db.String(120))
     join_at = db.Column(db.DateTime(), default=datetime.utcnow, index=True)
+
+    def __repr__(self):
+        return f"<User {self.username}>"
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',validators=[DataRequired()])
@@ -35,14 +41,14 @@ if exists('myUserDb.db') == False:
 @app.route('/')
 def index():
     current_users = User.query.all()
-    return render_template('index.html', users=current_users)
+    return render_template('index.html', current_users=current_users)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    form = RegistrationForm()
+    form = RegistrationForm(csrf_enabled=False)
     if form.validate_on_submit():
         user = User(username=form.username.data, email=form.email.data)
         user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
-    return render_template('register.html', form=form)
+    return render_template('register.html', title='Register', form=form)
