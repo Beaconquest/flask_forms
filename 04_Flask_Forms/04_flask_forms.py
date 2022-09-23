2698d3214dfc921565913ee1c1914523c7bcc777
Promise Mwenga <promise_mwenga@hotmail.com>
2022-09-23 00:25:30 -0300

    add login functions

--- 04_Flask_Forms/04_flask_forms.py
+++ 04_Flask_Forms/04_flask_forms.py
@@ -1,10 +1,10 @@
 from datetime import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
-from werkzeug.security import generate_password_hash
+from flask_login import UserMixin, LoginManager, login_required, login_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Email, EqualTo
 from os.path import exists
 
@@ -14,6 +14,9 @@ app.config['SECRET_KEY'] = 'SUPER-seCRet-Key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///myUserDB.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app()
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -28,12 +31,21 @@ class User(UserMixin, db.Model):
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
 class RegistrationForm(FlaskForm):
     username = StringField('Username',validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
 
 if exists('myUserDb.db') == False:
     db.create_all()
@@ -52,3 +64,29 @@ def register():
         db.session.add(user)
         db.session.commit()
     return render_template('register.html', title='Register', form=form)
+
+# user loader
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+# login route
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm(csrf_enabled=False)
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('index', _external=True, _scheme='https'))
+        else:
+            return redirect(url_for('login', _external=True, _scheme='https'))
+    return render_template('login.html', form=form)
+
+# user route
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('user.html', user=user)
