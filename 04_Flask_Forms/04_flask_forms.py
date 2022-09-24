13d9fd7d64faf32ad015038f006306457cbe501e
Promise Mwenga <promise_mwenga@hotmail.com>
2022-09-23 23:05:49 -0300

    added app theme and association user models

--- 04_Flask_Forms/04_flask_forms.py
+++ 04_Flask_Forms/04_flask_forms.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+from logging.config import valid_ident
+from wsgiref.validate import validator
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, LoginManager, login_required, login_user, current_user
@@ -34,6 +36,15 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+class Tournament(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String(140))
+    venue = db.Column(db.String(140))
+    weight_class = db.Column(db.String(140))
+    number_competitors = db.Column(db.Integer)
+    tournament_host_id = db.Column(db.Integer)
+    attendees = db.Column(db.String(256))
+
 class RegistrationForm(FlaskForm):
     username = StringField('Username',validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
@@ -47,14 +58,21 @@ class LoginForm(FlaskForm):
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
 
+class TournamentInfoForm(FlaskForm):
+    date = StringField('Date', validators=[DataRequired()])
+    venue = StringField('Venue', validators=[DataRequired()])
+    weight_class = StringField('Weight', validators=[DataRequired()])
+    number_of_competitors = StringField('Number of competitors')
+    submit = SubmitField('Create')
+
+class TournamentRSVPForm(FlaskForm):
+    tournament_id = StringField('Tournament Id', validators=[DataRequired()])
+    submit = SubmitField('Tournament RSVP')
+
 if exists('myUserDb.db') == False:
     db.create_all()
 
-@app.route('/')
-def index():
-    current_users = User.query.all()
-    return render_template('index.html', current_users=current_users)
-
+# register route
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm(csrf_enabled=False)
@@ -79,9 +97,9 @@ def login():
         if user and user.check_password(form.password.data):
             login_user(user, remember=form.remember.data)
             next_page = request.args.get('next')
-            return redirect(next_page) if next_page else redirect(url_for('index', _external=True, _scheme='https'))
+            return redirect(next_page) if next_page else redirect(url_for('index', _external=True, _scheme='http'))
         else:
-            return redirect(url_for('login', _external=True, _scheme='https'))
+            return redirect(url_for('login', _external=True, _scheme='http'))
     return render_template('login.html', form=form)
 
 # user route
@@ -89,4 +107,37 @@ def login():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    return render_template('user.html', user=user)
+    tournament_groups = Tournament.query.filter_by(tournamet_host_id=user.id)
+    if tournament_groups is None:
+        tournament_groups = []
+    form = TournamentInfoForm(csrf_enabled=False)
+    if form.validate_on_submit():
+        new_tournament_group = Tournament(
+            date = form.date.data,
+            venue = form.venue.data,
+            weight_class = form.weight_class.data,
+            number_of_competitors = form.number_of_competitors.data,
+            tournament_host_id = user.id,
+            attendees = username)
+        db.session.add(new_tournament_group)
+        db.session.commit()
+    return render_template('user.html',tournament_groups=tournament_groups, user=user, form=form)
+
+@app.route('/user/<username>/rsvp/', methods=['GET', 'POST'])
+@login_required
+def rvsp(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    tournament_groups = Tournament.query.all()
+    if tournament_groups is None:
+        tournament_groups =[]
+    form = TournamentRSVPForm(csrf_enabled=False)
+    if form.validate_on_submit():
+        tournament = Tournament.query.filter_by(id=int(form.tournament_id.data)).first()
+        tournament.attendees += f", {username}"
+        db.session.commit()
+    return render_template('rsvp.html', user=user, tournament_groups=tournament_groups, form=form)
+
+@app.route('/')
+def index():
+    current_users = User.query.all()
+    return render_template('index.html', current_users=current_users)
